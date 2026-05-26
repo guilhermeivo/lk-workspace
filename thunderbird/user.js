36efcaf0ac74cbcfe79812c61e5bfe6d69e6c310
Guilherme Ivo <guilhermeivob@gmail.com>
2026-05-26 03:15:04 -0300

    thunderbird: add kernel mailing list preferences
    Configure Thunderbird for Linux kernel mailing list workflows
    and plain text email handling.
    
    These settings improve compatibility with kernel-style email
    review and git send-email workflows.
    
    Signed-off-by: Guilherme Ivo <guilhermeivob@gmail.com>

---
+++ thunderbird/user.js
@@ -0,0 +1,39 @@
+// Mozilla User Preferences
+
+// If you make changes to this file while the application is running,
+// the changes will be overwritten when the application exits.
+
+user_pref("mail.identity.default.compose_html", false);
+user_pref("mail.quoteasblock", false);
+user_pref("mail.wrap_long_lines", false);
+user_pref("mail.compose.default_to_paragraph", false);
+
+// (0) mailnews.reply_header_originalmessage
+// -------- Original Message --------
+//
+// (1) authorwrote colon
+// Alf Aardvark wrote:
+//
+// (2) ondate separator authorwrote colon
+// On 01-01-2007 11:00 AM, Alf Aardvark wrote:
+//
+// (3) authorwrote separator ondate colon 
+// Alf Aardvark wrote, on 01-01-2007 11:00 AM: 
+//
+user_pref("mailnews.reply_header_type", 2);
+user_pref("mailnews.display.disallow_mime_handlers", 1);
+user_pref("mailnews.display.html_as", 1);
+user_pref("mailnews.display.prefer_plaintext", true);
+user_pref("mailnews.send_plaintext_flowed", false);
+user_pref("mailnews.wraplength", 72);
+
+user_pref("mailnews.default_sort_type", 18);
+user_pref("mailnews.default_sort_order", 2);
+user_pref("mailnews.default_view_flags", 1);
+
+user_pref("mailnews.tags.rfc.color", "#1a5fb4");
+user_pref("mailnews.tags.rfc.tag", "RFC");
+
+user_pref("plain_text.wrap_long_lines", false);
+
+user_pref("font.name.serif.x-western", "sans-serif");
